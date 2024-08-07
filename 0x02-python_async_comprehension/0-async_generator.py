f3b7eedfefc365bb198ee6997ce62412cdc85419
2Hasan2 <h01554176846@gmail.com>
2024-08-07 20:12:44 +0300

    2024-08-07 08:12:44 م

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """Async Generator"""
 import random
-from typing import AsyncGenerator
 import asyncio
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator():
     """Async Generator"""
     for _ in range(10):
         await asyncio.sleep(1)
